e015e3d477e365dcc551d8223a75d4a05d93c4c8
abobus1235345345 <139453779+abobus1235345345@users.noreply.github.com>
2024-04-03 00:49:16 +0300

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton_Click = new System.Windows.Forms.Button();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginButton_Click
+            // 
+            this.loginButton_Click.Location = new System.Drawing.Point(202, 465);
+            this.loginButton_Click.Name = "loginButton_Click";
+            this.loginButton_Click.Size = new System.Drawing.Size(75, 23);
+            this.loginButton_Click.TabIndex = 0;
+            this.loginButton_Click.Text = "ыыы";
+            this.loginButton_Click.UseVisualStyleBackColor = true;
+            this.loginButton_Click.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(212, 102);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(100, 20);
+            this.LoginTextBox.TabIndex = 1;
+            this.LoginTextBox.TextChanged += new System.EventHandler(this.LoginTextBox_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(212, 268);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(486, 533);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.loginButton_Click);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loginButton_Click;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = textBox4.Text;
+
+            Dictionary<string, string[]> employeesData = new Dictionary<string, string[]>()
+    {
+        {"Илья", new string[] {"Qwerty", "Илья", "Developer"}},
+        {"user2", new string[] { "user", "Jane Smith", "Manager"}}
+                
+    };
+
+
+            if (employeesData.ContainsKey(login) && employeesData[login][0] == password)
+            {
+                string name = employeesData[login][1];
+                string position = employeesData[login][2];
+
+                MessageBox.Show($"Имя: {name}\nДолжность: {position}", "Информация о сотруднике");
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка");
+            }
+        }
+
+        private void LoginTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+
+}
+
+        
+        
